6f19c1ceb1622e1ac9f6f3349d070a7f27cf6a36
malk <malk@zameth.org>
2016-08-19 16:07:25 +0200

    first commit

---
+++ src/main/java/App.java
@@ -0,0 +1,16 @@
+import static spark.Spark.*;
+
+public class App {
+
+    private static final String NL = System.lineSeparator();
+
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> {
+            return ln("0 0") + ln("100 100") + ln("200 300");
+        });
+    }
+
+    private static String ln(String line) {
+        return line + NL;
+    }
+}
